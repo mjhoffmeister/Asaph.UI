7700ced8880db5dca6327f6639389140473ef099
Matt Hoffmeister <matt.hoffmeister@gmail.com>
2022-07-27 13:34:39 -0500

    Add project files.

---
+++ Asaph.UI/Program.cs
@@ -0,0 +1,44 @@
+using Asaph.UI;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+IConfiguration configuration = builder.Build().Configuration;
+
+// Reference config values that will be used multiple times
+string backendApiBaseUrl = configuration["BackendApi:BaseUrl"];
+string backendApiScope = configuration["BackendApi:Scope"];
+
+// Add "Anonymous" HTTP client used for initially retrieving API documentation
+builder.Services.AddHttpClient(
+    "Anonymous",
+    client => client.BaseAddress = new Uri(backendApiBaseUrl));
+
+// Add "WebApi" HTTP client used for API requests and configure it to use access tokens
+builder.Services
+    .AddHttpClient("WebApi", client => client.BaseAddress = new Uri(backendApiBaseUrl))
+    .AddHttpMessageHandler(sp => sp.GetRequiredService<AuthorizationMessageHandler>()
+        .ConfigureHandler(
+            authorizedUrls: new[] { backendApiBaseUrl },
+            scopes: new[] { backendApiScope }));
+
+// Configure MSAL authentication
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAdb2c", options.ProviderOptions.Authentication);
+
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("openid");
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("offline_access");
+    options.ProviderOptions.DefaultAccessTokenScopes.Add(backendApiScope);
+
+    options.ProviderOptions.LoginMode = "redirect";
+});
+
+builder.Services.AddMudServices();
+
+await builder.Build().RunAsync();
